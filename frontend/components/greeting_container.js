a7a8a26eb75089d836967cac2a62cb0ed114bd24
Ed <edmundho9@gmail.com>
2018-06-04 09:30:12 -0700

    Complete greeting container

--- frontend/components/greeting_container.js
+++ frontend/components/greeting_container.js
@@ -2,12 +2,12 @@ import { connect } from 'react-redux';
 import {logoutUser} from '../actions/session_actions'
 import Greeting from './greeting';
 
-const mapStateToProps = ({ entities, session }, ownProps) => {
+const mapStateToProps = ({ entities, session }, ownProps) => ({
   currentUser: entities.users[session.id]
-};
+});
 
-const mapDispatchToProps = (dispatch) => {
+const mapDispatchToProps = (dispatch) => ({
   logout: () => dispatch(logoutUser())
-};
+});
 
 export default connect(mapStateToProps, mapDispatchToProps)(Greeting);
